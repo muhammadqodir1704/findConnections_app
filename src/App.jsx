84e6ec56105f243b731d678149b12bb943733946
Muhammadqodir <goldenparen06@gmail.com>
2025-04-28 00:14:56 +0500

    feat: added .env file

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import { UserProvider } from "./context/UserContext.jsx";
 import Graphics from "./graphVisulazer/Graphics";
 import SearchComponent from "./searchComponent/SearchComponent";
 import axios from "axios";
+
 const App = () => {
   const [message, setMessage] = useState("");
+  const API_URL = import.meta.env.VITE_API_URL; 
 
   useEffect(() => {
-    axios.get('http://localhost:5000/')
+    axios.get(`${API_URL}/`)
       .then(response => setMessage(response.data))
       .catch(error => console.error('Xatolik:', error));
   }, []);
+
   return (
     <UserProvider>
       <div className="max-w-5xl mx-auto p-6 space-y-6">
