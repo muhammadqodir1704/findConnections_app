55fa0edd01f94fe579faf291dce79fae670c82d2
Muhammadqodir <goldenparen06@gmail.com>
2025-05-24 17:45:39 +0500

    style:changed node colors

--- src/graphVisulazer/Graphics.jsx
+++ src/graphVisulazer/Graphics.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ForceGraph2D from "react-force-graph-2d";
 import { useUsers } from "../context/UserContext.jsx";
 import { findConnectionPath } from "../utils/findConnectionPath.js";
+import { BsHandIndex } from "react-icons/bs";
 
 const Graphics = ({ fromId, toId }) => {
   const { users } = useUsers();
@@ -10,16 +11,25 @@ const Graphics = ({ fromId, toId }) => {
   if (!users || users.length === 0) {
     return <div className="text-center p-4">Loading...</div>;
   }
+  const colors = [
+    "#f87171",
+    "#60a5fa",
+    "#34d399",
+    "#fbbf24",
+    "#a78bfa",
+    "#f472b6",
+  ];
 
-  const nodes = users.map(user => ({
+  const nodes = users.map((user ,index) => ({
     id: user.id,
     name: user.name,
+    color: colors[index % colors.length],
   }));
 
-  const links = users.flatMap(user =>
+  const links = users.flatMap((user) =>
     user.connections
-      .filter(connect => users.some(u => u.id === connect.target))
-      .map(connect => ({
+      .filter((connect) => users.some((u) => u.id === connect.target))
+      .map((connect) => ({
         source: user.id,
         target: connect.target,
         type: connect.type,
@@ -28,7 +38,8 @@ const Graphics = ({ fromId, toId }) => {
 
   const graphData = { nodes, links };
 
-  const allPaths = fromId && toId ? findConnectionPath(users, fromId, toId) : [];
+  const allPaths =
+    fromId && toId ? findConnectionPath(users, fromId, toId) : [];
   const highlightPath = allPaths && allPaths.length > 0 ? allPaths[0] : [];
 
   const highlightLinks = [];
@@ -39,36 +50,40 @@ const Graphics = ({ fromId, toId }) => {
     });
   }
   return (
-    <div className="h-[500px] border rounded-2xl overflow-hidden bg-white shadow-xl my-6 p-4 transition-all duration-300 hover:shadow-2xl">
-    <ForceGraph2D
-      graphData={graphData}
-      nodeLabel="name"
-      nodeAutoColorBy="id"
-      linkLabel="type"
-      linkDirectionalArrowLength={6}
-      linkDirectionalArrowRelPos={1}
-      linkColor={link =>
-        highlightLinks.some(
-          hl =>
-            (hl.source === link.source && hl.target === link.target) ||
-            (hl.source === link.target && hl.target === link.source)
-        )
-          ? "red"
-          : "#ccc"
-      }
-      nodeCanvasObject={(node, ctx, globalScale) => {
-        const label = node.name;
-        const fontSize = 14 / globalScale;
-        ctx.font = `bold ${fontSize}px Sans-Serif`;
-        ctx.fillStyle = highlightPath.includes(node.id) ? "#e11d48" : "#1f2937"; 
-        ctx.beginPath();
-        ctx.arc(node.x, node.y, 6, 0, 2 * Math.PI, false);
-        ctx.fill();
-        ctx.fillText(label, node.x + 8, node.y + 4);
-      }}
-    />
-  </div>
-  
+    <div className="h-[500px] border rounded-2xl overflow-hidden bg-white shadow-xl  transition-all duration-300 hover:shadow-2xl">
+      <ForceGraph2D
+        graphData={graphData}
+        nodeLabel="name"
+        nodeAutoColorBy="id"
+        linkLabel="type"
+        linkDirectionalArrowLength={6}
+        linkDirectionalArrowRelPos={1}
+        linkColor={(link) =>
+          highlightLinks.some(
+            (hl) =>
+              (hl.source === link.source && hl.target === link.target) ||
+              (hl.source === link.target && hl.target === link.source)
+          )
+            ? "red"
+            : "#ccc"
+        }
+        nodeCanvasObject={(node, ctx, globalScale) => {
+          const label = node.name;
+          const fontSize = 14 / globalScale;
+          ctx.font = `bold ${fontSize}px Sans-Serif`;
+          ctx.textAlign = "left";
+          ctx.textBaseline = "middle";
+
+          ctx.fillStyle = node.color; 
+
+          ctx.beginPath();
+          ctx.arc(node.x, node.y, 7, 0, 2 * Math.PI, false);
+          ctx.fill();
+
+          ctx.fillText(label, node.x + 10, node.y);
+        }}
+      />
+    </div>
   );
 };
 
